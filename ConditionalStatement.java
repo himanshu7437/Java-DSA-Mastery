3f6cdb1a98b0aac472dd058f5065c433ccea5d9d
himanshu7437 <himanshujangra368@gmail.com>
2025-01-12 22:37:02 +0530

    conditional statement added

---
+++ ConditionalStatement.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+
+public class ConditionalStatement {
+  public static void main(String[] args) {
+    // Create a Scanner object to read user input
+    Scanner sc = new Scanner(System.in);
+
+    // **Example 1: Checking Age (commented out)**
+    // This commented section demonstrates a simple if-else statement.
+    // Uncomment the lines below to see it in action.
+    // System.out.println("Enter your age:");
+    // int age = sc.nextInt();
+    //
+    // if (age > 18) {
+    //   System.out.println("You are an adult.");
+    // } else {
+    //   System.out.println("You are not an adult.");
+    // }
+
+    // **Example 2: Checking Even or Odd Number (commented out)**
+    // This commented section shows another if-else statement for checking
+    // if a number is even or odd.
+    // Uncomment the lines below to see it in action.
+    // System.out.println("Enter a number:");
+    // int number = sc.nextInt();
+    //
+    // if (number % 2 == 0) {
+    //   System.out.println("The number is even.");
+    // } else {
+    //   System.out.println("The number is odd.");
+    // }
+
+    // **Example 3: Comparing Two Numbers (commented out)**
+    // This commented section demonstrates nested if-else statements for
+    // comparing two numbers.
+    // Uncomment the lines below to see it in action.
+    // System.out.println("Enter the first number:");
+    // int a = sc.nextInt();
+    // System.out.println("Enter the second number:");
+    // int b = sc.nextInt();
+    //
+    // if (a == b) {
+    //   System.out.println("The numbers are equal.");
+    // } else {
+    //   if (a > b) {
+    //     System.out.println("The first number is bigger.");
+    //   } else {
+    //     System.out.println("The second number is bigger.");
+    //   }
+    // }
+
+    // **Multiple Conditions: if-else if vs. switch**
+    // The above examples used if-else statements for a few conditions.
+    // However, if you have many conditions to check, using a series of
+    // nested if-else statements can become cumbersome and difficult to read.
+
+    // **if-else if statement:**
+    // The `if-else if` construct allows you to chain multiple conditions
+    // together. The `else if` part is only checked if the previous `if`
+    // condition was false.
+
+    // **switch statement:**
+    // The `switch` statement provides a concise way to handle multiple
+    // conditions based on a single variable's value. It's often preferred
+    // for situations with many conditions to check.
+
+    // **Example: Selecting a Greeting (using switch)**
+//     System.out.println("Enter a number corresponding to a greeting (1 - hello, 2 - Namaste, 3 - Bonjour):");
+//     int choice = sc.nextInt();
+
+//     switch (choice) {
+//       case 1:
+//         System.out.println("Hello!");
+//         break;
+//       case 2:
+//         System.out.println("Namaste!");
+//         break;
+//       case 3:
+//         System.out.println("Bonjour!");
+//         break;
+//       default:
+//         System.out.println("Invalid greeting selection. Please enter 1, 2, or 3.");
+//     }
+  }
+}
