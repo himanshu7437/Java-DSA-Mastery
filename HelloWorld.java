54e766aadd600af6a3afcaaea4e0195dec0b617c
himanshu7437 <himanshujangra368@gmail.com>
2025-01-12 22:35:52 +0530

    hello world added

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// This is a basic Java program that prints "Hello, World!" to the console.
+public class HelloWorld {
+
+    // The main method is the entry point of every Java application.
+    public static void main(String[] args) {
+        // Printing "Hello, World!" to the console.
+        System.out.println("Hello, World!!");
+    }
+}
