ec3b97ec43d58805a2c490d01977405a4759f6ec
himanshu7437 <himanshujangra368@gmail.com>
2025-01-18 21:42:25 +0530

    inverted pyramid and diamond

--- PatternQuestions1.java
+++ PatternQuestions1.java
@@ -45,6 +45,41 @@ public class PatternQuestions1 {
       }
       System.out.println();
   
+      // **5. Inverted Pyramid**
+      System.out.println("5. Inverted Pyramid:");
+      for (int i = n; i >= 1; i--) {
+        for (int j = 1; j <= n - i; j++) {
+          System.out.print("  "); // Print spaces
+        }
+        for (int j = 1; j <= (2 * i - 1); j++) {
+          System.out.print("* ");
+        }
+        System.out.println();
+      }
+      System.out.println();
+  
+      // **6. Diamond**
+      System.out.println("6. Diamond:");
+      for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+          System.out.print("  "); // Print spaces
+        }
+        for (int j = 1; j <= (2 * i - 1); j++) {
+          System.out.print("* ");
+        }
+        System.out.println();
+      }
+      for (int i = n - 1; i >= 1; i--) {
+        for (int j = 1; j <= n - i; j++) {
+          System.out.print("  "); // Print spaces
+        }
+        for (int j = 1; j <= (2 * i - 1); j++) {
+          System.out.print("* ");
+        }
+        System.out.println();
+      }
+      System.out.println();
+  
       
     }
   }
