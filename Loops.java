cf128b10c37f7363e3d26c1113078b255dfd2683
himanshu7437 <himanshujangra368@gmail.com>
2025-01-18 21:33:03 +0530

    while loop

--- Loops.java
+++ Loops.java
@@ -12,8 +12,19 @@ public class Loops {
       System.out.println("Number: " + i);
     }
 
-    
+    // **Example 2: Calculating Sum with a While Loop**
+    // The `while` loop is used when we don't know the number of iterations beforehand, but we have a condition to check.
+    System.out.println("\nExample 2: Enter numbers to calculate their sum. Enter -1 to stop:");
+    int sum = 0;
+    int num = sc.nextInt();
+    while (num != -1) { // Loop continues until the user enters -1
+      sum += num;
+      System.out.println("Current Sum: " + sum);
+      num = sc.nextInt();
+    }
+    System.out.println("Final Sum: " + sum);
 
+    
     // Closing the Scanner
     sc.close();
   }
