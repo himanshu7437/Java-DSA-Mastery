02d20c6fec056f946d413d73518b37f0a9183333
himanshu7437 <himanshujangra368@gmail.com>
2025-01-22 19:09:23 -0300

    string builder added

---
+++ stringBuilder.java
@@ -0,0 +1,53 @@
+public class stringBuilder {
+    public static void main(String[] args) {
+        
+        //stringbuilder
+
+        // StringBuilder sb = new StringBuilder("Tony");
+        // System.out.println(sb);
+        
+        // // charAt index 0
+        // System.out.println(sb.charAt(0));
+
+        // //setchar at index 0
+        // sb.setCharAt(0, 'p');
+        //  System.out.println(sb);
+
+        //  //insert a index i
+        //  sb.insert(0, 'S');
+        //  System.out.println(sb);
+
+        //  sb.insert(2, 'n');
+        //  System.out.println(sb);
+
+        //  // delete the extra s
+        //  sb.delete(0, 1);
+        //  System.out.println(sb);
+
+        //  // apend
+        //  sb.append(" is");
+        //  sb.append(" a good boy.");
+        //  System.out.println(sb);
+        //  System.out.println(sb.length());
+
+        // ques - reverse a string
+        // StringBuilder name1 = new StringBuilder("hello");
+
+        // for (int i = 0; i < name1.length()/2; i++) {
+           
+        //     int front = i;
+        //     int back = name1.length() - 1 - i;
+
+        //     char frontchar = name1.charAt(front);
+        //     char backchar = name1.charAt(back);
+
+        //     name1.setCharAt(front, backchar);
+        //     name1.setCharAt(back, frontchar);
+        // }
+        // System.out.println(name1);
+
+        
+
+
+    }
+}
