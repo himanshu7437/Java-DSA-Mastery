ba86a28f1b7675c41cf002317d4bfceeec8fd5f0
himanshu7437 <himanshujangra368@gmail.com>
2025-01-24 23:33:16 +0530

    insertion sort

--- sortinginjava.java
+++ sortinginjava.java
@@ -39,7 +39,27 @@ public class sortinginjava {
         //     System.out.print(arr[i] + " ");
         // }
 
-        // 
+        // insertion sort
+
+        // int arr[] = {3,5,2,7,6,9};
+
+        // for (int i = 1; i < arr.length; i++) {
+        //     int current = arr[i];
+        //     int j =i- 1;
+
+        //     while (j>=0 && current < arr[j]) {
+        //         arr[j+1] = arr[j];
+        //         j--;
+        //     }
+        //     arr[j+1] = current;
+        // }
+
+        // for (int i = 0; i < arr.length; i++) {
+        //     System.out.println("the array is " + arr[i]);
+        // }
+
+        
+
 
 
     }
