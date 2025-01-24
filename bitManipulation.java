be21287b74d7c7d64031bf67a2b9dd0b58b7911f
himanshu7437 <himanshujangra368@gmail.com>
2025-01-24 03:01:25 -0300

    bit manulation updated

--- bitManipulation.java
+++ bitManipulation.java
@@ -1,5 +1,50 @@
+
 public class bitManipulation {
     public static void main(String[] args) {
-        
+        // get,set,update,delete bit operation (bit manipulation)
+
+        // 1. get
+
+        // int n = 5;
+        // int bitmask=1<<3;
+
+        // if ((n&bitmask)==0) {
+        //     System.out.println("it is zero");
+        // } else {
+        //     System.out.println("it is one.");
+        // }
+
+
+        // set 
+
+        // int n = 5;
+        // int bitmask=1<<1;
+        // int result = n|bitmask;
+
+        // System.out.println("this is "+result);
+
+        // clear 
+
+        // int n = 5;
+        // int bitmask=1<<2;
+        // int notbitmask = ~(bitmask);
+        // int result = n & notbitmask;
+
+        // System.out.println("the number is"+ result);
+
+        //update
+        // Scanner sc = new Scanner(System.in);
+        // int n = 5;
+        // int bitmask=1<<1;
+        // int choice = sc.nextInt();
+
+        // if (choice==0) {
+        //     int notbitmask = ~(bitmask);
+        //     int result = n % notbitmask;
+        //     System.out.println("the result is "+ result);
+        // } else if (choice == 1) {
+        //     int result = n | bitmask;
+        //     System.out.println("the answer is " + result);
+        // }
     }
 }
