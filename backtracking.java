b0ff74d6c755a2f8b38f95525d54f06e94ce0f6e
himanshu7437 <himanshujangra368@gmail.com>
2025-01-26 02:17:47 +0530

    backtracking

---
+++ backtracking.java
@@ -0,0 +1,26 @@
+public class backtracking {
+
+    // public static void printpurmutation(String str, String perm, int idx) {
+    //     if(str.length()==0) {
+    //         System.out.println(perm);
+    //         return;
+    //     }
+    //     for (int i = 0; i < str.length(); i++) {
+    //         char currChar = str.charAt(i);
+    //         String newStr = str.substring(0,i) + str.substring(i+1);
+    //         printpurmutation(newStr, perm+currChar, idx+i);
+
+    //     }
+
+
+    // }
+    public static void main(String[] args) {
+        
+        // backtracking
+
+        // String str = "abc";
+        // printpurmutation(str, "", 0);
+
+        
+    }
+}
