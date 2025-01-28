196cdc130a0e0ba5a15bc16f125a646bf00e5186
himanshu7437 <himanshujangra368@gmail.com>
2025-01-28 22:06:39 +0530

    single level inheritance

---
+++ OOPs3.java
@@ -0,0 +1,17 @@
+class Shape {  // base class or parent class
+    String color;
+}
+
+class Triangle extends Shape {
+// child class or sub class
+}
+
+public class OOPs3 {
+    public static void main(String[] args) {
+        
+        //inheritance
+        Triangle t1 = new Triangle();
+        t1.color = "red";
+
+    }
+}
---
+++ OOPs4.java
@@ -0,0 +1,23 @@
+class Shape {  // base class or parent class
+    public void area () {
+        System.out.println("display area");
+    }
+}
+
+class Triangle extends Shape {
+// child class or sub class
+
+public void area (int l, int h) {
+    System.out.println(1/2*l*h);
+}
+}
+
+public class OOPs4 {
+    public static void main(String[] args) {
+        
+        //inheritance
+
+        // types 
+        // 1. single level inheritance
+    }
+}
