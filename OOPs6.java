461e5596cb55a2681076094c2a6367609e7137b7
himanshu7437 <himanshujangra368@gmail.com>
2025-01-28 22:12:01 +0530

    multi level inheritance

---
+++ OOPs6.java
@@ -0,0 +1,29 @@
+class Shape {  // base class or parent class
+    public void area () {
+        System.out.println("display area");
+    }
+}
+
+class Triangle extends Shape {
+// child class or sub class
+
+public void area (int l, int h) {
+    System.out.println(1/2*l*h);
+}
+}
+
+class EquilateralTriangle extends Triangle {
+    public void area (int l, int h) {
+        System.out.println(1/2*l*h);
+    }
+}
+
+public class OOPs6 {
+    public static void main(String[] args) {
+        
+        //inheritance
+
+        // types 
+        // 2. Multi-level inheritance
+    }
+}
