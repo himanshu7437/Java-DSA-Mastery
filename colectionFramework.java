b03c1783f2639b1eaad09db9a8323af0b2a1d532
himanshu7437 <himanshujangra368@gmail.com>
2025-01-29 15:13:36 +0530

    ppt link added

--- colectionFramework.java
+++ colectionFramework.java
@@ -3,5 +3,7 @@ public class colectionFramework {
     public static void main(String[] args) {
         
         // Java collection frameworks
+        // drive link for ppt:-
+        // https://drive.google.com/file/d/1Vde0ztULzoBZtdCflLHGi7dMMtTM9Oi8/view
     }
 }
