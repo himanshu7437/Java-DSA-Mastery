8298ef89ea557060c3c37ec8a43294e7420853c9
himanshu7437 <himanshujangra368@gmail.com>
2025-01-30 19:12:06 +0530

    questions of linked list

---
+++ linkedList3.java
@@ -0,0 +1,23 @@
+
+import java.util.LinkedList;
+
+public class linkedList3 {
+    
+    public static void main(String[] args) {
+        
+        // linked list questions
+
+        // ques - 
+
+        LinkedList<String> list = new LinkedList<String>();
+
+        list.add("my");
+        list.add("name");
+        list.add("is");
+        list.add("himanshu");
+
+        System.out.println(list);
+
+        int n = 2;
+    }
+}
