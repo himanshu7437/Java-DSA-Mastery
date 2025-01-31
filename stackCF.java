0ee2dd66e1b367a752282dc50c9b751c2335a761
himanshu7437 <himanshujangra368@gmail.com>
2025-01-31 15:12:37 +0530

    2 questions added related to stack

--- stackCF.java
+++ stackCF.java
@@ -1,17 +1,71 @@
 import java.util.*;
 
 public class stackCF {
+
+    //pushAtBottom
+
+    public static void pushAtBottom(int data, Stack<Integer> s) {
+        if(s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(data, s);
+        s.push(top);
+    }
+
+    // reverse
+
+    public static void reverse(Stack<Integer> s) {
+        if(s.isEmpty()) {
+            return;
+        }
+        int top = s.pop();
+        reverse(s);
+        pushAtBottom(top, s);
+        
+    }
+
     
     public static void main(String[] args) {
         
         // java collection framework
 
+        // Stack<Integer> s = new Stack<>();
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
+        // s.push(4);
+        // s.push(5);
+
+        // while (!s.isEmpty()) { 
+        //     System.out.println(s.peek());
+        //     s.pop();
+        // }
+
+        // ques - push at the bottom of stack
+
+        // Stack<Integer> s = new Stack<>();
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
+
+
+        // pushAtBottom(4 , s);
+
+        // while (!s.isEmpty()) { 
+        //     System.out.println(s.peek());
+        //     s.pop();
+        // }
+
+        // ques - reverse a stack
+        
         Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
-        s.push(4);
-        s.push(5);
+
+        reverse(s);
 
         while (!s.isEmpty()) { 
             System.out.println(s.peek());
