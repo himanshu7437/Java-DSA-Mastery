1f6cbd638d41838a8814ab2ee6118834893b9872
himanshu7437 <himanshujangra368@gmail.com>
2025-02-01 19:15:22 +0530

    queue using linked List

--- queues1.java
+++ queues1.java
@@ -1,10 +1,11 @@
 
 
-public class queues {
+public class queues1 {
     static class Queue {
         static int arr[];
         static int size;
         static int rear = -1;
+        static int front = -1;
 
         Queue(int n) {
             arr = new int[n];
@@ -12,32 +13,43 @@ public class queues {
         }
 
         public static boolean isEmpty()  {
-            return rear == -1;
+            return rear == -1 && front == -1;
+        }
+
+        public static boolean isFull()  {
+            return (rear+1)%size == front;
         }
 
         //enqueue
         public static void add(int data) {
-            if(rear == size ) {
+            if(isFull()) {
                 System.out.println("queue is full.");
                 return;
             }
 
-            rear++;
+            // 1st element add 
+            if ( front == -1) {
+                front = 0;
+            }
+            rear = (rear + 1) % size;
             arr[rear] = data;
         }
 
-        //dequeue - O(n)
+        //dequeue - O(1)
         public static int remove() {
             if (isEmpty()) {
+                System.out.println("empty queue.");
                 return-1;
             }
 
-            int front = arr[0];
-            for (int i = 0; i < rear; i++) {
-                arr[i] = arr[i+1];
+            int result = arr[front];
+            // single element condition
+            if(rear == front) {
+                rear = front = -1;
+            } else {
+                front = (front + 1) % size;
             }
-            rear--;
-            return front;
+            return result;
         }
 
         //peek
@@ -47,18 +59,24 @@ public class queues {
                 return-1;
             }
 
-            return  arr[0];
+            return  arr[front];
         }
 
 
     }
     public static void main(String[] args) {
         
-        //Queues
+        //Circular Queue using array
         Queue q = new Queue(5);
         q.add(1);
         q.add(2);
         q.add(3);
+        q.add(4);
+        q.add(5);
+        System.out.println(q.remove());
+        q.add(6);
+        System.out.println(q.remove());
+        q.add(7);
 
         while (!q.isEmpty()) { 
             System.out.println(q.peek());
---
+++ queues3.java
@@ -0,0 +1,22 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class queues3 {
+    
+    public static void main(String[] args) {
+        
+        // queue using java collection frameWork
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5); 
+
+        while (!q.isEmpty()) { 
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
