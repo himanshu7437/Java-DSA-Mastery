2d8afdb9deeb070120a00fa028f9998a60ca086b
himanshu7437 <himanshujangra368@gmail.com>
2025-02-01 19:27:32 +0530

    code added

--- queues3.java
+++ queues3.java
@@ -1,4 +1,4 @@
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class queues3 {
@@ -7,7 +7,9 @@ public class queues3 {
         
         // queue using java collection frameWork
 
-        Queue<Integer> q = new LinkedList<>();
+        // Queue q = new Queue();
+        // Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new ArrayDeque<>();
         q.add(1);
         q.add(2);
         q.add(3);
