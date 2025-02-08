1de2d45cfeaa3125c4f43a2b8a2bf75feb4b1f23
himanshu7437 <himanshujangra368@gmail.com>
2025-02-08 13:32:58 +0530

    hashset - insert,  delete, iterator, etc added

--- hashSetInJava.java
+++ hashSetInJava.java
@@ -1,7 +1,48 @@
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+
+
 public class hashSetInJava {
     
     public static void main(String[] args) {
         
         // Hashset in java
+        HashSet <Integer> set = new HashSet<>();
+
+        // insert
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(1);
+
+        //size
+        System.out.println("size of the set is "+ set.size());
+
+        //print all the elements
+        System.out.println(set);
+
+        //iterator 
+        Iterator it = set.iterator();
+        
+        while (it.hasNext()) { 
+            System.out.println(it.next());
+        }
+
+
+        // search - contans
+        // if(set.contains(1)){
+        //     System.out.println("set contains 1");
+        // }
+        // if(!set.contains(6)){
+        //     System.out.println("set doesn't contains 6 ");
+        // }
+
+        // //delete
+        // set.remove(1);
+        // if(!set.contains(1)){
+        //     System.out.println("set doesn't contains 1.");
+        // }
     }
 }
